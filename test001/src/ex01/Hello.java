35b60cb7bdbaa3a4310d8f789ce1103daae435b9
Ji-Soo-JANG <wltn2101@gmail.com>
2024-08-07 10:55:35 +0900

    충돌 해결

--- test001/src/ex01/Hello.java
+++ test001/src/ex01/Hello.java
@@ -3,11 +3,7 @@ package ex01;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
-<<<<<<< HEAD
-		System.out.println("hello2");
-=======
 		System.out.println("Hi2");
 		System.out.println("Hi3");
->>>>>>> branch 'master' of https://github.com/Ji-Soo-JANG/test001.git
 	}
 }
