308e6cdfc3cef0b7a73762d2775e95db9e58bdc2
Ji-Soo-JANG <wltn2101@gmail.com>
2024-08-07 11:43:06 +0900

    Hello.java 수정

--- test001/src/ex01/Hello.java
+++ test001/src/ex01/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Hi2");
-		System.out.println("Hi3");
+		System.out.println("Hi");
 	}
 }
